c38484d3f669d506fc26af76a0dd60e186a9a4f0
ArthurPereira026 <arthurpereiraoliveira026@gmail.com>
2026-06-15 17:40:40 -0300

    sofrendo mais um pouco no python

--- dicionario/atividades/ativ05.py
+++ dicionario/atividades/ativ05.py
@@ -1,2 +1,10 @@
 palavras = {}
 
+frase =  input("Digite frase aqui: ").split()
+for n in frase:
+    if n not in palavras:
+        palavras[n] = 1
+    else:
+        palavras[n] += 1
+for i,j in palavras.items():
+    print(f"{i} -> {j}")
---
+++ dicionario/atividades/ativ06.py
@@ -0,0 +1,38 @@
+estoque = {
+    "Teclado": 15,
+    "Mouse": 22,
+    "Monitor": 8
+}
+print(estoque)
+continuar = "s"
+while continuar == "s":
+    nome, quantidade = input("""
+     Bem Vindos a Café impresso ☕
+     Digite o Nome do Produto e a Quantidade que deseja comprar,
+     separando-a por virgula: """).split(",")
+    retorno = estoque.get(nome, "Produto n encontrado!")
+
+    for chave, valor in estoque.items():
+        if nome.lower() == chave.lower():
+            if valor == 0:
+                print("Infelizmente o estoque desse produto estar esgotado😥😥😥")
+                continue
+            if valor < int(quantidade):
+                print("Possuímos esse item no estoque,Mas infelizmente temos menos do que desejas")
+                continue
+            else:
+                estoque[chave] -= int(quantidade)
+                atualiza_estoque = True
+
+    if atualiza_estoque:
+        print("Este é o nosso estoque atual atualizado!")
+        for chave, valor in estoque.items():
+            print(f"{chave} -> {valor}")
+
+    continuar = input("Desejas Continuar a sua compra ou quer ir para o carrinho? (s/n): ").lower()
+else:
+    print("Agradecemos por comprar na Café impresso ☕")
+
+
+
+
